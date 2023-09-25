c36c0b304cf3a4e277d22ffa8367744d0a29936d
EthanDenyer <ex21178@qmul.ac.uk>
2023-09-25 11:28:24 +0100

    update

---
+++ SOURCECODE.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+import requests
+from datetime import datetime
+import xlwings as xw
+
+def HTMLGRABBER():  #We need to grab the HTML here so that we can extract columns for data
+
+
